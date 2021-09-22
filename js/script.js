c88a35403875ea0a1a0377765aad8733384ea3ae
Martín Calderón <martincalderon92@outlook.com>
2021-09-22 17:22:35 -0300

    :sparkles: Add search suggestions to the input.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import './suggestions.js';
+
 let weatherData = undefined;
 
 getWeatherData();
---
+++ js/suggestions.js
@@ -0,0 +1,29 @@
+const searchInput = document.getElementById('searchInput');
+const suggestionsList = document.getElementById('suggestions');
+let suggestions = undefined;
+
+searchInput.addEventListener('keyup', getSuggestions);
+
+function getSuggestions(e) {
+	suggestionsList.innerHTML = '';
+	let queryUrl = `https://api.allorigins.win/raw?url=https://www.metaweather.com/api/location/search/?query=`;
+
+	if (searchInput.value.length > 0) {
+		fetch(queryUrl + searchInput.value)
+			.then((data) => data.json())
+			.then((data) => {
+				suggestions = data;
+				showSuggestions();
+			});
+	}
+}
+
+function showSuggestions() {
+	for (let i = 0; i < 5; i++) {
+		let item = suggestions[i];
+
+		if (item) {
+			suggestionsList.innerHTML += `<li>${item.title}</li>`;
+		}
+	}
+}
