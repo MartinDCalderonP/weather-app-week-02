c02176d8a512bfea0e81964707a7fc43870a0fdf
Martín Calderón <martincalderon92@outlook.com>
2021-09-23 20:01:53 -0300

    ♻️ Refactor showWeatherData function.

--- js/script.js
+++ js/script.js
@@ -1,59 +1,40 @@
 'use strict';
 
 import './suggestions.js';
-import { getDayName } from './helpers.js';
-
-let weatherData;
+import './search.js';
+import { getWeatherData, getDayName } from './helpers.js';
 
 getWeatherData();
 
-function getWeatherData() {
-	fetch(
-		'https://api.allorigins.win/raw?url=https://www.metaweather.com/api/location/2487956/'
-	)
-		.then((data) => data.json())
-		.then((data) => {
-			weatherData = data;
-			updater();
-		})
-		.catch((err) => {
-			console.log(err);
-		});
-}
-
-function updater() {
-	showMainTitle();
-	showCards();
-}
-
-function showMainTitle() {
+export function showWeatherData(weatherData) {
 	const mainTitle = document.getElementById('mainTitle');
-
-	mainTitle.innerHTML += `5 Day Weather - ${weatherData.title}`;
-}
-
-function showCards() {
 	const cardContainer = document.getElementById('cardContainer');
+
+	mainTitle.innerHTML = `5 Day Weather - ${weatherData.title}`;
+
+	let cards = '';
 
 	for (let i = 0; i < weatherData.consolidated_weather.length - 1; i++) {
 		let item = weatherData.consolidated_weather[i];
 		let imageUrl = `https://www.metaweather.com/static/img/weather/${item.weather_state_abbr}.svg`;
 
-		cardContainer.innerHTML += `<div class="card-container__card">
-										<h2>${getDayName(item.applicable_date)}</h2>
-										<img
-											class="card-container__card__weather-icon"
-											src="${imageUrl}"
-											alt="${item.weather_state_name}"
-										/>
-										<div>
-											<h3>${Math.round(item.the_temp)}°C</h3>
-											<p>${item.weather_state_name}</p>
-										</div>
-										<div>
-											<p>Min: ${Math.round(item.min_temp)}°C</p>
-											<p>Max: ${Math.round(item.max_temp)}°C</p>
-										</div>
-									</%div>`;
+		cards += `<div class="card-container__card">
+					<h2>${getDayName(item.applicable_date)}</h2>
+					<img
+						class="card-container__card__weather-icon"
+						src="${imageUrl}"
+						alt="${item.weather_state_name}"
+					/>
+					<div>
+						<h3>${Math.round(item.the_temp)}°C</h3>
+						<p>${item.weather_state_name}</p>
+					</div>
+					<div>
+						<p>Min: ${Math.round(item.min_temp)}°C</p>
+						<p>Max: ${Math.round(item.max_temp)}°C</p>
+					</div>
+				</div>`;
 	}
+
+	cardContainer.innerHTML = cards;
 }
