b4beed8237e80c2ad585a59ff0903a8636418138
Martín Calderón <martincalderon92@outlook.com>
2021-09-23 17:05:39 -0300

    :sparkles: Create throotle function on helpers.

--- js/helpers.js
+++ js/helpers.js
@@ -19,3 +19,18 @@ export function debounce(fn, delay) {
 		}, delay);
 	};
 }
+
+export function throttle(fn, delay) {
+	let last = 0;
+
+	return function (...args) {
+		const now = new Date().getTime();
+
+		if (now - last < delay) {
+			return;
+		}
+
+		last = now;
+		return fn(...args);
+	};
+}
