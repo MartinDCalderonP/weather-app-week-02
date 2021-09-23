6f58bef97273fbe3330f73156682e0ffb3b98b74
Martín Calderón <martincalderon92@outlook.com>
2021-09-23 19:26:06 -0300

    ✨Add search location feature.

---
+++ js/search.js
@@ -0,0 +1,37 @@
+import { getWeatherData } from './script.js';
+import { throttle } from './helpers.js';
+
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+let searchResults;
+
+searchButton.addEventListener('click', throttle(searchLocation, 5000));
+
+function searchLocation(e) {
+	e.preventDefault();
+	pauseSearchButton();
+
+	let queryUrl = `https://api.allorigins.win/raw?url=https://www.metaweather.com/api/location/search/?query=`;
+
+	fetch(queryUrl + searchInput.value)
+		.then((data) => data.json())
+		.then((data) => {
+			searchResults = data;
+			showSearchResults();
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+}
+
+function showSearchResults() {
+	if (searchResults[0].woeid) {
+		getWeatherData(searchResults[0].woeid);
+	}
+}
+
+function pauseSearchButton() {
+	searchButton.disabled = true;
+
+	setTimeout(() => (searchButton.disabled = false), 5000);
+}
