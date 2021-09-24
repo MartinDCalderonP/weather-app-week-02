82ed19fdb54ec1857ec3028259cc83fac3e7dfb8
Martín Calderón <martincalderon92@outlook.com>
2021-09-24 17:37:32 -0300

    💫 Add animations when cards appears.

--- js/script.js
+++ js/script.js
@@ -20,27 +20,29 @@ function getLastOrDefaultData() {
 
 export function showWeatherData(weatherData) {
 	mainTitle.innerHTML = `5 Day Weather - ${weatherData.title}`;
-	cardContainer.innerHTML = '';
+	let cards = '';
 
 	for (let i = 0; i < weatherData.consolidated_weather.length - 1; i++) {
 		let item = weatherData.consolidated_weather[i];
 		let imageUrl = `https://www.metaweather.com/static/img/weather/${item.weather_state_abbr}.svg`;
 
-		cardContainer.innerHTML += `<div class="card-container__card">
-										<h2>${getDayName(item.applicable_date)}</h2>
-										<img
-											class="card-container__card__weather-icon"
-											src="${imageUrl}"
-											alt="${item.weather_state_name}"
-										/>
-										<div>
-											<h3>${Math.round(item.the_temp)}°C</h3>
-											<p>${item.weather_state_name}</p>
-										</div>
-										<div>
-											<p>Min: ${Math.round(item.min_temp)}°C</p>
-											<p>Max: ${Math.round(item.max_temp)}°C</p>
-										</div>
-									</div>`;
+		cards += `<div class="card-container__card appear-card-${[i + 1]}">
+					<h2>${getDayName(item.applicable_date)}</h2>
+					<img
+						class="card-container__card__weather-icon"
+						src="${imageUrl}"
+						alt="${item.weather_state_name}"
+					/>
+					<div>
+						<h3>${Math.round(item.the_temp)}°C</h3>
+						<p>${item.weather_state_name}</p>
+					</div>
+					<div>
+						<p>Min: ${Math.round(item.min_temp)}°C</p>
+						<p>Max: ${Math.round(item.max_temp)}°C</p>
+					</div>
+				</div>`;
 	}
+
+	cardContainer.innerHTML = cards;
 }
