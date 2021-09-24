b915672ed305ac23b22b56bd49ee734984e5289d
Martín Calderón <martincalderon92@outlook.com>
2021-09-24 17:12:47 -0300

    ✨Add input focus in/out effects.

---
+++ js/eventListeners.js
@@ -0,0 +1,14 @@
+searchInput.addEventListener('focusin', inputFocusIn);
+searchInput.addEventListener('focusout', inputFocusOut);
+
+function inputFocusIn() {
+	searchInputDiv.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.15)';
+	searchInputDiv.style.borderColor = 'darkblue';
+	searchButton.style.color = 'darkblue';
+}
+
+function inputFocusOut() {
+	searchInputDiv.style.boxShadow = 'none';
+	searchInputDiv.style.borderColor = 'var(--lightblue)';
+	searchButton.style.color = 'var(--lightblue)';
+}
