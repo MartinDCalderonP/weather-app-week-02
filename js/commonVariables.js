a8823e6fa8d80055221902ffab5aa1bd301b464d
Martín Calderón <martincalderon92@outlook.com>
2021-09-23 21:49:45 -0300

    :art: Create a file for common variables.

---
+++ js/commonVariables.js
@@ -0,0 +1,5 @@
+export const mainTitle = document.getElementById('mainTitle');
+export const cardContainer = document.getElementById('cardContainer');
+export const searchInput = document.getElementById('searchInput');
+export const suggestionsList = document.getElementById('suggestionsList');
+export const searchButton = document.getElementById('searchButton');
