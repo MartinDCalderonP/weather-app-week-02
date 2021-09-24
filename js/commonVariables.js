e990034de2febd03d362d3ee0068949262d8c53f
Martín Calderón <martincalderon92@outlook.com>
2021-09-23 22:45:33 -0300

    :art: Moved more variables to common variables file.

--- js/commonVariables.js
+++ js/commonVariables.js
@@ -3,3 +3,7 @@ export const cardContainer = document.getElementById('cardContainer');
 export const searchInput = document.getElementById('searchInput');
 export const suggestionsList = document.getElementById('suggestionsList');
 export const searchButton = document.getElementById('searchButton');
+
+export const weatherAPIUrl = `https://api.allorigins.win/raw?url=https://www.metaweather.com/api/location/`;
+export const searchInWeatherAPIUrl = `search/?query=`;
+export const defaultId = `2487956`;
