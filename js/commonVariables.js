9d84475cfa614d4f6865e8c6a2920dc8133cbbc5
Martín Calderón <martincalderon92@outlook.com>
2021-09-29 12:47:22 -0300

    🩹 Delete AllOrigins API url.

--- js/commonVariables.js
+++ js/commonVariables.js
@@ -6,6 +6,6 @@ export const searchInput = document.getElementById('searchInput');
 export const suggestionsList = document.getElementById('suggestionsList');
 export const searchButton = document.getElementById('searchButton');
 
-export const weatherDataUrl = `https://api.allorigins.win/raw?url=https://www.metaweather.com/api/location/`;
+export const weatherDataUrl = `https://www.metaweather.com/api/location/`;
 export const searchInWeatherDataUrl = `search/?query=`;
 export const defaultId = `2487956`;
